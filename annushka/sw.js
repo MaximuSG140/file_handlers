336d0cb9b8bda202430b03cc693b692fc6bf99e3
Anna Velichko <anna-velichko@yandex-team.ru>
2021-09-09 17:52:48 +0700

    Fix sw

--- annushka/sw.js
+++ annushka/sw.js
@@ -3,18 +3,20 @@ const urlsToCache = [
   'index.html',
 ];
 
+self.addEventListener('message' , m => {
+  console.log('meow');
+  yandex.messenger.isPanelEnabled((is_enabled) => {
+    chrome.runtime.lastError ? console.log(chrome.runtime.lastError.message) :
+                               console.log(is_enabled);
+  });
+});
+
 self.addEventListener('install', e => {
   e.waitUntil(
       caches.open('basic-cache').then((cache) => {
         return cache.addAll(urlsToCache);
       }),
   );
-
-  console.log('meow');
-  yandex.messenger.isPanelEnabled((is_enabled) => {
-    chrome.runtime.lastError ? console.log(chrome.runtime.lastError.message) :
-                               console.log(is_enabled);
-  });
 });
 
 self.addEventListener('fetch', e => {
@@ -26,10 +28,4 @@ self.addEventListener('fetch', e => {
         return fetch(e.request);
       }),
   );
-
-  console.log('meow');
-  yandex.messenger.isPanelEnabled((is_enabled) => {
-    chrome.runtime.lastError ? console.log(chrome.runtime.lastError.message) :
-                               console.log(is_enabled);
-  });
 });
